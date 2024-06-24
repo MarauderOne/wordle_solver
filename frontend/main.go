7e72e9b60769af57c7b75b63a5365b7e63db6ea0
Alexander Berridge <alexjberridge@gmail.com>
2024-06-24 14:54:45 +0100

    First working version where the UI is plumbed into the backend

--- frontend/main.go
+++ frontend/main.go
@@ -1,47 +1,141 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+    "github.com/MarauderOne/wordle_solver/dictionary_tools"
+	"github.com/gin-gonic/gin"
+    "strings"
 )
 
 func main() {
-    r := gin.Default()
 
-    // Serve static files
-    r.Static("/static", "./static")
+    tmp := []BoxData{
+        {Character:"",Color:""},
+        {Character:"t",Color:"green"},
+    }
+    solve(tmp)
 
-    // Define your routes
-    r.GET("/", func(c *gin.Context) {
-        c.File("./static/index.html")
-    })
+	r := gin.Default()
 
-    // Endpoint to handle Wordle solving
-    r.POST("/solve", solveWordle)
+	// Serve static files
+	r.Static("/static", "./static")
 
-    // Run the server
-    r.Run(":8080")
+	// Define your routes
+	r.GET("/", func(c *gin.Context) {
+		c.File("./static/index.html")
+	})
+
+	// Endpoint to handle Wordle solving
+	r.POST("/solve", solveWordle)
+
+	// Run the server
+	r.Run(":8080")
+}
+
+type BoxData struct {
+    Character string `json:"character"`
+    Color     string `json:"color"`
 }
 
 func solveWordle(c *gin.Context) {
-    // Parse input
-    var input struct {
-        Guess string `json:"guess"`
-    }
-    if err := c.ShouldBindJSON(&input); err != nil {
-        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-        return
-    }
+	var gridData []BoxData
+	if err := c.ShouldBindJSON(&gridData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-    // Call your Wordle solving function (implement this)
-    result := solve(input.Guess)
+	// Call your Wordle solving function (implement this)
+	result := solve(gridData)
 
-    // Respond with the result
-    c.JSON(http.StatusOK, gin.H{"result": result})
+	// Respond with the result
+	c.JSON(http.StatusOK, gin.H{"result": result})
 }
+
+
 
 // Placeholder for your Wordle solving logic
-func solve(guess string) string {
-    // Implement your logic here
-    return "solution"
+func solve(guess []BoxData) string {
+
+    //Initialise answer list
+    answerList := createNewAnswerList()
+
+    //Start looping through the user boxes
+    for i, box := range guess {
+
+    var greenRegex string
+    var yellowRegex string
+    var greyRegex string
+
+        //Assign postition based on index number in the array (i)
+        switch i {
+        case 0, 5, 10, 15, 20, 25:
+            //Character in first position
+            greenRegex = fmt.Sprintf("%v....", box.Character)
+            yellowRegex = fmt.Sprintf("[^%v][%v{1,}]...$|^[^%v].[%v{1,}]..$|^[^%v]..[%v{1,}].$|^[^%v]...[%v{1,}]", box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character)
+            greyRegex = fmt.Sprintf("[^%v]....", box.Character)
+        case 1, 6, 11, 16, 21, 26:
+            //Character in second position
+            greenRegex = fmt.Sprintf(".%v...", box.Character)
+            yellowRegex = fmt.Sprintf("[%v{1,}][^%v]...$|^.[^%v][%v{1,}]..$|^.[^%v].[%v{1,}].$|^.[^%v]..[%v{1,}]", box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character)
+            greyRegex = fmt.Sprintf(".[^%v]...", box.Character)
+        case 2, 7, 12, 17, 22, 27:
+            //Character in third position
+            greenRegex = fmt.Sprintf("..%v..", box.Character)
+            yellowRegex = fmt.Sprintf("[%v{1,}].[^%v]..$|^.[%v{1,}][^%v]..$|^..[^%v][%v{1,}].$|^..[^%v].[%v{1,}]", box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character)
+            greyRegex = fmt.Sprintf("..[^%v]..", box.Character)
+        case 3, 8, 13, 18, 23, 28:
+            //Character in fourth position
+            greenRegex = fmt.Sprintf(".%v.", box.Character)
+            yellowRegex = fmt.Sprintf("[%v{1,}]..[^%v].$|^.[%v{1,}].[^%v].$|^..[%v{1,}][^%v].$|^...[^%v][%v{1,}]", box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character)
+            greyRegex = fmt.Sprintf("...[^%v].", box.Character)
+        case 4, 9, 14, 19, 24, 29:
+            //Character in fifth position
+            greenRegex = fmt.Sprintf("....%v", box.Character)
+            yellowRegex = fmt.Sprintf("[%v{1,}]...[^%v]$|^.[%v{1,}]..[^%v]$|^...[%v{1,}].[^%v]$|^...[%v{1,}][^%v]", box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character, box.Character)
+            greyRegex = fmt.Sprintf("....[^%v]", box.Character)
+        }
+
+
+        if (box.Character == "") || (box.Color == "") {
+            //Skip over boxes which have wither no character or no color
+            continue
+            //ToDo: Add logging
+        } else if box.Color == "green" {
+            //Find matches for character in position
+		    answerList = reviseAnswerList(answerList, greenRegex)
+
+            } else if box.Color == "yellow" {
+                //Find matches for character anywhere but in position
+                answerList = reviseAnswerList(answerList, yellowRegex)
+
+                } else if box.Color == "grey" {
+                    //Find matches for character not position
+                    answerList = reviseAnswerList(answerList, greyRegex)
+
+                } else {
+                //Invalid color
+        }
+    }
+
+    //return fmt.Sprint("End of function")
+    var resultSummary string = fmt.Sprintf("\nThe list of possible answers has been reduced to: %v\n", answerList.Count())
+    results := strings.Join(answerList.Words, " ")
+    return resultSummary + results
 }
+
+//Define a function to initialise the answerList
+func createNewAnswerList() (dictionary *dictionary_tools.MySimpleDict) {
+	d := dictionary_tools.NewSimpleDict()
+	d.Load("../dictionary_tools/initialList.dict")
+	return d
+}
+
+//Define a function to revise the answerList based on given regex patterns
+func reviseAnswerList(answersList *dictionary_tools.MySimpleDict, regexPattern string) (revisedDictionary *dictionary_tools.MySimpleDict) {
+	newAnswerList := answersList.Lookup(regexPattern, 0, 6000)
+	d := dictionary_tools.NewSimpleDict()
+	d.AddWordsList(newAnswerList)
+	return d
+}
+
